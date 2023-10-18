5117f2d63e1d564355ff2569b14bd07c87da3108
elias <elias.giesinger@student.htldornbirn.at>
2023-10-18 11:19:54 +0200

    fjwk

--- src/at/sma/e/g/basics/ControlStructurs3.java
+++ src/at/sma/e/g/basics/ControlStructurs3.java
@@ -4,24 +4,33 @@ import java.util.Random;
 
 public class ControlStructurs3 {
     public static void main(String[] args) {
-        // Erstelle eine Zufallszahl zwischen 0 und 100
         Random random = new Random();
-        int randomNumber = random.nextInt(100);
+        int randomNumber = random.nextInt(5);
 
-        if(randomNumber<20){
-            System.out.println("Mini");
-        }else if((randomNumber>20) && (randomNumber<50)){
-            System.out.println("Medium");
-        }else if(randomNumber>50){
-            System.out.println("Large");
+        switch (randomNumber){
+            case 0:
+                System.out.println("is 0");
+                break;
+
+            case 1:
+                System.out.println("is 1");
+                break;
+
+            case 2:
+                System.out.println("is 2");
+                break;
+
+            default:
+                System.out.println("other value");
+                break;
         }
 
-        // Gib die Zufallszahl aus
+        // Erstelle eine Zufallszahl zwischen 5 und 10
 
-        // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
-        // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
-        // Wenn die Zahl größer als 50 ist gib aus "Large"
-
+        // Wenn der Wert 10 ist gib aus Ten
+        // Wenn der Wert 9 ist gib aus Nine
+        // Wenn der Wert 8 ist gib aus Eight
+        // etc.
 
     }
 }
