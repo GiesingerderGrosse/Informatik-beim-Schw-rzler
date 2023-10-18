cd2b6cc3e95504feffd7f3be116e1a3b7b65f41d
elias <elias.giesinger@student.htldornbirn.at>
2023-10-18 11:06:01 +0200

    fjwk

--- src/at/sma/e/g/basics/ControlStructurs4.java
+++ src/at/sma/e/g/basics/ControlStructurs4.java
@@ -4,8 +4,19 @@ import java.util.Random;
 
 public class ControlStructurs4 {
     public static void main(String[] args) {
+
+        Random random = new Random();
+        int randomNumber1 = random.nextInt(100);
+        int randomNumber2 = random.nextInt(100);
         // Erstelle zwei Zufallszahl zwischen 0 und 100
 
+        if((randomNumber1<randomNumber2) && (randomNumber1<50)){
+            System.out.println("Zahl 1 ist kleiner als Zahl 2 und Mini");
+        }else if((randomNumber1<30) || (randomNumber2<30)){
+            System.out.println("Eine der beiden ist kleiner als 30");
+        }else if((randomNumber1<50) && (randomNumber2!=50)){
+            System.out.println("Erste Zahl klein, zweite kein 50iger");
+        }
         // Wenn die erste Zahl kleiner ist als die zweite UND die erste Zahl kleiner ist als 50
         // dann gib aus "Zahl 1 ist kleiner als Zahl 2 und Mini"
 
