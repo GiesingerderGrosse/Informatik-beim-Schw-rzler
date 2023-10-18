f02994cf4c4e827c50b7200d008c8be831a0cada
Elias Giesinger <elias.giesinger@student.htldornbirn.at>
2023-10-18 10:21:21 +0200

    Start

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo");
+
+        //short - ganze zahl (kleinnnnnnnnnnnnn)
+        //int - ganze zahl
+        //float - kommazahl
+        //double - kommazahl(großßßßßßßßßßßßß)
+        //boolean - true/false
+
+    }
+
+    //strg blank
+}
