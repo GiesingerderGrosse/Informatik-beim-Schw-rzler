573372a8d719fe283f239b363428a61a0ad68627
elias <elias.giesinger@student.htldornbirn.at>
2023-10-25 11:28:28 +0200

    fjwk

--- src/at/sma/e/g/basics/ControlStructurs3.java
+++ src/at/sma/e/g/basics/ControlStructurs3.java
@@ -1,32 +1,55 @@
 package at.sma.e.g.basics;
 
+import javax.sound.midi.Soundbank;
 import java.util.Random;
+import java.util.Scanner;
 
 public class ControlStructurs3 {
+
     public static void main(String[] args) {
 
-        Random random = new Random();
-        int Summe = 0;
-        boolean Stopp = false;
+        Scanner scanner = new Scanner(System.in);
+        int Kontostand = 0;
+
+        System.out.println("Bitte wählen Sie eines der vier Möglichkeiten aus:");
+        System.out.println("1. Einzahlen");
+        System.out.println("2. Auszahlen");
+        System.out.println("3. Kontostand");
+        System.out.println("4. Beenden");
+
+        int Auswahl = Integer.parseInt(scanner.next());
+
+        switch(Auswahl){
+
+            case 1:{
+                System.out.println("Bitte geben Sie den Betrag ein, den Sie einzahlen möchten:");
+                int Eingabe = Integer.parseInt(scanner.next());
+                Kontostand = Eingabe + Kontostand;
+                break;
+            }
+            case 2:{
+                System.out.println("Bitte geben Sie den Betrag ein, den Sie auszahlen möchten:");
+                int Ausgabe = Integer.parseInt(scanner.next());
+
+                if (Ausgabe < Kontostand){
+                    System.out.println("Sie besitzen nicht genügend Geld!");
+                    System.out.println("Kontostand:");
+                    System.out.println(Kontostand);
+                }else{
+                    Kontostand = Kontostand - Ausgabe;
+                }
+            break;
+            } case 3:{
+                System.out.println("Ihr aktueller Kontostand beträgt:");
+                System.out.println(Kontostand);
+                break;
+            } case 4:{
+                break;
+            } default:{
+                System.out.println("Falsche Eingabe.");
+            }
 
 
-    while (!Stopp){
-
-        int randomValue = random.nextInt(20);
-        randomValue = randomValue + 10;
-
-        if (randomValue == 15){
-            Stopp = true;
-        }else if (randomValue == 25){
-            Stopp = true;
-        }else{
-            Summe = Summe + randomValue;
         }
-
-    }
-        System.out.println(Summe);
-
-
-
     }
 }
