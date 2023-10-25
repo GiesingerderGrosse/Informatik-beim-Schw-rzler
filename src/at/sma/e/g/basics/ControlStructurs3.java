e549c9023a159d476661e3b12537af321c92864d
elias <elias.giesinger@student.htldornbirn.at>
2023-10-25 10:30:40 +0200

    fjwk

--- src/at/sma/e/g/basics/ControlStructurs3.java
+++ src/at/sma/e/g/basics/ControlStructurs3.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class ControlStructurs3 {
     public static void main(String[] args) {
 
-        int Summe = 0;
-        for (int i = 0; i<=100; i++){
+        int Summe = 2;
+        for (int i = 0; i<=1000; i=i+2){
 
         Summe = Summe + i;
 
@@ -14,13 +14,6 @@ public class ControlStructurs3 {
         System.out.println(Summe);
 
 
-
-        // Erstelle eine Zufallszahl zwischen 5 und 10
-
-        // Wenn der Wert 10 ist gib aus Ten
-        // Wenn der Wert 9 ist gib aus Nine
-        // Wenn der Wert 8 ist gib aus Eight
-        // etc.
 
     }
 }
