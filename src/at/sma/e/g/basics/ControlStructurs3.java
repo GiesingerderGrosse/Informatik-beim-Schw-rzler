fad956f0e55552e8db2b0ad9727bbb0a90992963
elias <elias.giesinger@student.htldornbirn.at>
2023-11-08 10:03:19 +0100

    fjwk

--- src/at/sma/e/g/basics/ControlStructurs3.java
+++ src/at/sma/e/g/basics/ControlStructurs3.java
@@ -10,44 +10,50 @@ public class ControlStructurs3 {
 
         Scanner scanner = new Scanner(System.in);
         int Kontostand = 0;
+        boolean isFinished = true;
 
-        System.out.println("Bitte wählen Sie eines der vier Möglichkeiten aus:");
-        System.out.println("1. Einzahlen");
-        System.out.println("2. Auszahlen");
-        System.out.println("3. Kontostand");
-        System.out.println("4. Beenden");
+        while (isFinished){
+            System.out.println("Bitte wählen Sie eines der vier Möglichkeiten aus:");
+            System.out.println("1. Einzahlen");
+            System.out.println("2. Auszahlen");
+            System.out.println("3. Kontostand");
+            System.out.println("4. Beenden");
 
-        int Auswahl = Integer.parseInt(scanner.next());
+            int Auswahl = Integer.parseInt(scanner.next());
 
-        switch(Auswahl){
+            switch(Auswahl){
 
-            case 1:{
-                System.out.println("Bitte geben Sie den Betrag ein, den Sie einzahlen möchten:");
-                int Eingabe = Integer.parseInt(scanner.next());
-                Kontostand = Eingabe + Kontostand;
-                break;
-            }
-            case 2:{
-                System.out.println("Bitte geben Sie den Betrag ein, den Sie auszahlen möchten:");
-                int Ausgabe = Integer.parseInt(scanner.next());
-
-                if (Ausgabe < Kontostand){
-                    System.out.println("Sie besitzen nicht genügend Geld!");
-                    System.out.println("Kontostand:");
-                    System.out.println(Kontostand);
-                }else{
-                    Kontostand = Kontostand - Ausgabe;
+                case 1:{
+                    System.out.println("Bitte geben Sie den Betrag ein, den Sie einzahlen möchten:");
+                    int Eingabe = Integer.parseInt(scanner.next());
+                    Kontostand = Eingabe + Kontostand;
+                    break;
                 }
-            break;
-            } case 3:{
-                System.out.println("Ihr aktueller Kontostand beträgt:");
-                System.out.println(Kontostand);
-                break;
-            } case 4:{
-                break;
-            } default:{
-                System.out.println("Falsche Eingabe.");
-            }
+                case 2:{
+                    System.out.println("Bitte geben Sie den Betrag ein, den Sie auszahlen möchten:");
+                    int Ausgabe = Integer.parseInt(scanner.next());
+
+                    if (Ausgabe > Kontostand){
+                        System.out.println("Sie besitzen nicht genügend Geld!");
+                        System.out.println("Kontostand:");
+                        System.out.println(Kontostand);
+                    }else{
+                        Kontostand = Kontostand - Ausgabe;
+                    }
+                    break;
+                } case 3:{
+                    System.out.println("Ihr aktueller Kontostand beträgt:");
+                    System.out.println(Kontostand);
+                    break;
+                } case 4:{
+                    System.out.println("Programm Beendet.");
+                    isFinished = false;
+                    break;
+                } default:{
+                    System.out.println("Falsche Eingabe.");
+                }
+        }
+
 
 
         }
