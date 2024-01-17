2be6ef65c68389cc5e3af499beec9c7f9ea48d98
elias <elias.giesinger@student.htldornbirn.at>
2024-01-17 11:26:48 +0100

    Projektstart Projekt "Haus"

--- src/at/sma/e/g/basics/oop/haus/Strasse.java
+++ src/at/sma/e/g/basics/oop/haus/Strasse.java
@@ -1,4 +1,8 @@
 package at.sma.e.g.basics.oop.haus;
-
+import java.util.ArrayList;
 public class Strasse {
+    private String sName;
+    private int ilaenge;
+    private int iAnzahlGehsteige;
+
 }
